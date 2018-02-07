3684c4126e5d4b5d35c535ace33f641cee0f87ff
Viktor Zhurbin <vzhurbin@gmail.com>
2018-02-07 19:06:13 +0300

    brand new pagination

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,67 +1,58 @@
-export const pagination = (rowsPerPage = 7) => {
-  const table = document.querySelector('table');
-  const rowCount = table.rows.length; // number of rows in the table
+export const pagination2 = (rowsPerPage) => {
+  
+  // const form = document.querySelector('form');
+  // const input = document.querySelector('input[type=search]');
+  // const button = document.querySelector('button.search');
+  const table = document.getElementById('table');
+  const trArray = [...table.querySelectorAll('tr.row')];
+  const pageCount = Math.ceil(trArray.length / rowsPerPage);
+  console.log(pageCount);
+  
   const firstRowTag = table.rows[0].firstElementChild.tagName;
-  // boolean var to check if table has a head row
-  const hasHead = (firstRowTag === 'TH');
+  const hasHeader = (firstRowTag === 'TH');
   // start from 2nd row if the first row is header
-  const firstDataRow = (hasHead) ? 1 : 0;
-  // holds the first row if it has a (<TH>) & nothing if (<TD>)
-  const header = (hasHead) ? table.rows[(0)].outerHTML : '';
-  // count the number of pages
-  const pageCount = Math.ceil(rowCount / rowsPerPage);
-  // an array to hold each row
-  let tr = [];
+  // const firstDataRow = (hasHeader ? 0 : 1);
+  
+  const visibleRows = (pageNum = 1) => {
+    console.log('visible rows');
+    trArray.forEach((tr, i) => {
+      const isHeader = tr.firstElementChild.tagName === 'TH';
+      const startRow = (rowsPerPage * pageNum) - rowsPerPage;
+      const endRow = rowsPerPage * pageNum - 1;
+      
+      if (isHeader || (i >= startRow && i <= endRow)) {
+        tr.classList.remove('hidden');
+      } else {
+        tr.classList.add('hidden');
+      }
+    });
+  };
 
-  // if we had one page only; then we have nothing to do ..
-  if (pageCount > 1) {
-    // assign each row outHTML (tag name & innerHTML) to the array
-    for (let i = firstDataRow, j = 0; i < rowCount; i++ , j++) {
-      tr[j] = table.rows[i].outerHTML;
-    }
-    // console.log(tr);
-    // create a div block to hold the buttons
+  const pageButtons = (pageCount = 1) => {
     table.insertAdjacentHTML('afterend', '<div id="buttons"></div');
-    // the first sort; default page is the first one
-    sort(1);
-  }
-
-  // (pageNum) is the selected page number. it will be generated when a user clicks a button
-  function sort(pageNum) {
-    let rows = header;
-    const startRow = ((rowsPerPage * pageNum) - rowsPerPage);
-    for (let i = startRow; i < (startRow + rowsPerPage) && i < tr.length; i++) {
-      rows += tr[i];
-    }
-
-    // now the table has a processed group of rows
-    table.innerHTML = rows;
-
-    // create pagination buttons
-    const buttonsDiv = document.getElementById('buttons');
-    const buttonsHtml = pageButtons(pageCount);
-    buttonsDiv.innerHTML = buttonsHtml;
-
-    for (let i = 1; i < pageCount; i++) {
-      const btn = document.getElementById(`page-${i}`);
-      btn.addEventListener('click', e => sort(i));
-    }
-  }
-
-  function pageButtons(pageCount) {
-   /* disable the 'Prev' button on 1st page
-      and 'Next' button on last page 
-      const	disablePrevious = (cur === 1) ? 'disabled' : '';
-      const disableNext = (cur === pCount) ? 'disabled' : '';
-    */
+    /* disable the 'Prev' button on 1st page
+    and 'Next' button on last page 
+    const	disablePrevious = (cur === 1) ? 'disabled' : '';
+    const disableNext = (cur === pCount) ? 'disabled' : '';
+     */
     // let buttons = 
     // `<input type="button" value="&lt;&lt; Prev" id="page-0" onclick="sort(${cur - 1})" ${prevDis}>`;
     let buttons = '';
-    for (let i = 1; i < pageCount; i++) {
-      buttons += `<input type="button" id="page-${i}" value="${i}">`;
+    for (let i = 1; i <= pageCount; i++) {
+      buttons += `<input type="button" id="page-btn-${i}" value="${i}">`;
     }
     // buttons += `<input type="button" value="Next &gt;&gt;" id="page-${i + 1}" onclick="sort(${(cur + 1)})" ${nextDis}>`;
+    
+    const buttonsDiv = document.getElementById('buttons');
+    buttonsDiv.innerHTML = buttons;
+    
+    for (let i = 1; i <= pageCount; i++) {
+      const btn = document.getElementById(`page-btn-${i}`);
+      btn.addEventListener('click', e => visibleRows(i));
+    }
+    console.log(buttonsDiv);
+   }
 
-    return buttons;
-  }
-}
+   pageButtons(pageCount);
+   visibleRows(1);
+};
